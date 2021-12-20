27b938d62c30444ea784f7bbb074233d55259b9b
= <judson.fp.jr@gmail.com>
2021-12-20 12:49:12 -0300

    Finished RadialProgress.lua

--- src/JJUI/RadialProgress.lua
+++ src/JJUI/RadialProgress.lua
@@ -5,12 +5,12 @@ local Create = require(script.Parent.System.Create)
 local function createSelf()
     -- Frame
 	local frame = Create("Frame", {
-        AnchorPoint = UDim.new(.5, .5);
+        AnchorPoint = Vector2.new(.5, .5);
 		BackgroundTransparency = 1;
-		Size = UDim2.new(0.1,0, 0.162,0);
+		Size = UDim2.new(0.113,0, 0.184,0);
 	})
 	frame.Name = "template"
-	frame.Parent = script
+    --frame.Parent = script
 
     -- Left
     local leftimg = Create("ImageLabel", {
@@ -20,10 +20,30 @@ local function createSelf()
         ZIndex = 2;
         Image = "rbxassetid://8295839524";
         ImageColor3 = Color3.fromRGB(50,50,50);
-        ImageRectSize = UDim.new(128,256);
+        ImageRectSize = Vector2.new(128,256);
+        ScaleType = Enum.ScaleType.Fit;
     })
     leftimg.Name = "Left"
     leftimg.Parent = frame
+
+    local leftsubframe = Create("Frame", {
+        AnchorPoint = Vector2.new(.5, .5);
+        BackgroundTransparency = 1;
+        Position = UDim2.new(1,0, .5,0);
+        Size = UDim2.new(2,0, 1,0);
+    })
+    leftsubframe.Parent = leftimg
+
+    local leftsubframe_subimg = Create("ImageLabel", {
+        BackgroundTransparency = 1;
+        Position = UDim2.new(0,0, 0,0);
+        Size = UDim2.new(.5,0, 1,0);
+        Image = "rbxassetid://8295840780";
+        ImageRectSize = Vector2.new(128,256);
+        ImageColor3 = Color3.fromRGB(55,155,255);
+        ScaleType = Enum.ScaleType.Fit;
+    })
+    leftsubframe_subimg.Parent = leftsubframe
 
     -- Right
     local rightimg = Create("ImageLabel", {
@@ -33,18 +53,57 @@ local function createSelf()
         ZIndex = 1;
         Image = "rbxassetid://8295839524";
         ImageColor3 = Color3.fromRGB(50,50,50);
-        ImageRectOffset = UDim.new(128,0);
-        ImageRectSize = UDim.new(128,256);
+        ImageRectOffset = Vector2.new(128,0);
+        ImageRectSize = Vector2.new(128,256);
+        ScaleType = Enum.ScaleType.Fit;
     })
     rightimg.Name = "Right"
     rightimg.Parent = frame
 
+    local rightsubframe = Create("Frame", {
+        AnchorPoint = Vector2.new(.5, .5);
+        BackgroundTransparency = 1;
+        Position = UDim2.new(0,0, .5,0);
+        Size = UDim2.new(2,0, 1,0);
+    })
+    rightsubframe.Parent = rightimg
 
+    local rightsubframe_subimg = Create("ImageLabel", {
+        BackgroundTransparency = 1;
+        Position = UDim2.new(.5,0, 0,0);
+        Size = UDim2.new(.5,0, 1,0);
+        Image = "rbxassetid://8295840780";
+        ImageRectOffset = Vector2.new(128,0);
+        ImageRectSize = Vector2.new(128,256);
+        ImageColor3 = Color3.fromRGB(55,155,255);
+        ScaleType = Enum.ScaleType.Fit;
+    })
+    rightsubframe_subimg.Parent = rightsubframe
+
+    frame.Parent = script
 end
 
 createSelf()
 
 local TweenService = game:GetService("TweenService")
+local function update(self)
+    local value = (self.progress/100) * 360
+
+    local left = self.ui.Left.Frame
+    local right = self.ui.Right.Frame
+
+    local t1 = TweenService:Create(right, TweenInfo.new(self.anim_s), {Rotation = math.clamp(value-180, -180, 0)})
+    t1:Play()
+    --right.Rotation = math.clamp(value - 180, -180, 0)
+    if value > 180 then
+		left.Visible = true
+        local t2 = TweenService:Create(left, TweenInfo.new(self.anim_s), {Rotation = math.clamp(value-360, -180, 0)})
+        t2:Play()
+		--left.Rotation = math.clamp(value - 360, -180, 0)
+	else
+		left.Visible = false
+	end
+end
 
 function RadialProgress.new(Name, Position, ColorsConfig, AnimationSpeed)
 	assert(type(Name) == "string", "Name argument must be a string.")
@@ -58,6 +117,9 @@ function RadialProgress.new(Name, Position, ColorsConfig, AnimationSpeed)
 		--// UI
 		ui = script.template:Clone();
         anim_s = AnimationSpeed or .15;
+
+        --// Values
+        progress = 0;
 	}, RadialProgress)
 	
 	--// Setup
@@ -68,6 +130,14 @@ function RadialProgress.new(Name, Position, ColorsConfig, AnimationSpeed)
 	return self
 end
 
+--// From class
+function RadialProgress:SetProgress(percent)
+    self.progress = percent
+    self.progress = self.progress % 100
+    update(self)
+end
+
+--// From instance
 function RadialProgress:SetPosition(pos)
 	self.ui.Position = pos
 end
--- src/JJUI/init.lua
+++ src/JJUI/init.lua
@@ -2,4 +2,5 @@ return {
 	Switch = require(script.Switch);
 	DragBar = require(script.DragBar);
 	Button = require(script.Button);
+	RadialProgress = require(script.RadialProgress);
 }
